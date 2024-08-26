a07ac6db1f246c07a87a040ac06286552869a5ad
Alexandra Baukhina <Baukhina88@yandex.ru>
2024-08-26 21:43:53 +0300

    one

---
+++ src/masks.py
@@ -0,0 +1,32 @@
+def get_mask_card_number(card_number: str) -> str:
+    ''' Функция принимает на вход номер карты и возвращает ее маску'''
+    # Проверим, что длина номера карты правильная
+    if len(card_number) != 16:
+        raise ValueError("Номер карты должен содержать 16 цифр")
+
+    # Разделяем номер карты на блоки по 4 цифры
+    blocks = [card_number[i : i + 4] for i in range(0, 16, 4)]
+
+    # Формируем маску
+    masked_blocks = [
+        blocks[0],  # Первые 4 цифры
+        blocks[1][:2] + "**",  # Следующие 2 цифры видны, остальное заменено на **
+        "****",  # Третий блок полностью заменён на ****
+        blocks[3],  # Последние 4 цифры
+    ]
+
+    # Объединяем блоки в строку с пробелами
+    masked_number = " ".join(masked_blocks)
+
+    return masked_number
+
+
+def get_mask_account(account_number: str) -> str:
+    ''' Функция принимает на вход номер счета и возвращает его маску'''
+    # Оставляем последние шесть символов номера счета
+    acc_num = account_number[-6:]
+
+    # Формируем маску
+    masked_number = "**" + acc_num[-4:]
+
+    return masked_number
