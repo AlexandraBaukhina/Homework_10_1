74d7d938f00bfc3455afd9d8c5b4b922d7bb00c0
Alexandra Baukhina <Baukhina88@yandex.ru>
2024-09-24 23:53:10 +0300

    домашка 12.2 - logging, надо только flake8 сделать

--- src/utils.py
+++ src/utils.py
@@ -1,17 +1,26 @@
 import json
+import logging
+
+logger = logging.getLogger('utils')
+logger.setLevel(logging.INFO)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s: %(message)s')
+file_handler.setFormatter(file_formatter)
+logger.addHandler(file_handler)
 
 
 def read_transactions_from_json(file_path):
     try:
+        logger.info(f'Записываем данные в файл {file_path}')
         with open(file_path, 'r') as file:
             data = json.load(file)
             if isinstance(data, list):
                 return data
             else:
                 return []
-    except FileNotFoundError:
-        print("File not found")
+    except FileNotFoundError as e:
+        logger.error(f'Произошла ошибка: {e}')
         return []
-    except json.JSONDecodeError:
-        print("Invalid JSON file")
+    except json.JSONDecodeError as e:
+        logger.error(f'Произошла ошибка: {e}')
         return []
